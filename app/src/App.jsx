5f0e8742f25b9061a1bc6021a5703f8edc4092b8
Gabriel <stewarti3@yahoo.com>
2025-02-13 00:33:36 +0100

    feat: Navbar

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './index.css'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='flex bg-[#02191D] w-full min-h-screen px-16 py-8 justify-center'>
+      <NavBar />
+
+    </div>
   )
 }
 
---
+++ app/src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaArrowRightLong } from 'react-icons/fa6'
+
+const NavBar = () => {
+  return (
+    <
+      div
+      className="flex flex-row h-[4.75rem] w-3/4 bg-[#05252C] bg-opacity-[40%] rounded-[1.5rem] bg-clip-padding backdrop-filter backdrop-blur-sm border border-[#197686] fixed items-center py-[0.75rem] px-[1rem]">
+      <div className="grid grid-cols-3 grid-rows-1 items-center gap-14">
+        <a href="#" className='mr-auto'>
+          <img src="/logo.svg" alt="logo" className='h-full w-auto' />
+        </a>
+        <div className="flex flex-row text-[1.125rem] text-[#B3B3B3] gap-6 text-center mx-auto">
+          <a href="#" className='hover:text-[#FFF]'>Events</a>
+          <a href="#" className='hover:text-[#FFF]'>My Tickets</a>
+          <a href="#" className='hover:text-[#FFF]'>About Project</a>
+        </div>
+        <a href="#" className='ml-auto'>
+          <button className='inline-flex px-[1.5rem] h-[3.25rem] text-[#0A0C11] hover:text-white bg-white hover:bg-[#24A0B5] justify-center items-center rounded-xl border border-[#D9D9D9] gap-2 text-[1rem] group transition-all duration-300'>
+            MY TICKETS
+            <FaArrowRightLong className='group-hover:-rotate-45' />
+          </button>
+        </a>
+      </div>
+
+    </div>
+  )
+}
+
+export default NavBar
